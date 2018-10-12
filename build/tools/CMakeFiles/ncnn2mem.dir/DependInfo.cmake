7afb956f2a4c4db95a0f8d701c1c33e0d0105f0e
零尾 <lwplwf@qq.com>
2018-10-12 14:46:25 +0800

    Add files via upload

---
+++ build/tools/CMakeFiles/ncnn2mem.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bu5/bu5project/ncnn/tools/ncnn2mem.cpp" "/home/bu5/bu5project/ncnn/build/tools/CMakeFiles/ncnn2mem.dir/ncnn2mem.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../tools/../src"
+  "tools/../src"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/bu5/bu5project/ncnn/build/src/CMakeFiles/ncnn.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
